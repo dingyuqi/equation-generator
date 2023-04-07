1f40ce49f2ac380ef1951855b5a6e1971019090a
dingyuqi <dingyuqi@sics.ac.cn>
2023-04-07 18:54:37 +0800

    可以正常请求

--- backen/app.py
+++ backen/app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, render_template, request, make_response, jsonify
 
 app = Flask(__name__, static_url_path='', static_folder='../front', template_folder='../front')
@@ -13,9 +15,15 @@ def greet():
 
 @app.route('/run', methods=['POST'])
 def run():
-    print("!!!!!!!!!!!!!", request.method, request.method == "POST", request.get_json())
     if request.method == "POST":
-        print("1111", request.get_json())
-        if request.get_json()["file_path"]:  # 判断是否为空
-            print("2222", request.get_json()["file_path"], "=========")
-            return jsonify({'status': True})
+        data = json.loads(request.get_data())
+        if data["file_path"]:  # 判断是否为空
+            return jsonify({
+                'code': 200,
+                'data': "",
+                'msg': 'success'})
+        else:
+            return jsonify({
+                'code': 404,
+                'data': "",
+                'msg': 'failed'})
